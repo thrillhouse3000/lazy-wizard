e977d545bd4eefcfb2b3f0fa063b5ef614b49606
Erik Dever <erikdever@hotmail.com>
2022-05-24 18:41:30 -0600

    Updated DB url

--- app.py
+++ app.py
@@ -13,7 +13,7 @@ from route_functions import already_logged_in, login_required, is_author, author
 
 app = Flask(__name__)
 
-app.config['SQLALCHEMY_DATABASE_URI'] = (os.environ.get('DATABASE_URL', 'postgresql:///lazy_wizard'))
+app.config['SQLALCHEMY_DATABASE_URI'] = (os.environ.get('DATABASE_URL'.replace("://", "ql://", 1), 'postgresql:///lazy_wizard'))
 app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
 app.config['SQLALCHEMY_ECHO'] = True
 app.config['SECRET_KEY'] = os.environ.get('SECRET_KEY', "keepitsecretkeepitsafe")
